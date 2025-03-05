93e447c6ae1331ae25a2f19eb94613773d9eacd9
Logan Brown <logan.brown@tylertech.com>
2025-03-04 20:48:46 -0500

    Add project files.

---
+++ Library.cs
@@ -0,0 +1,304 @@
+﻿using MelonLoader;
+using UnityEngine;
+using Repo_Library;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+[assembly: MelonInfo(typeof(Library), "R.E.P.O Mod Library", "1.0.0", "Lillious & .Zer0")]
+[assembly: MelonGame("semiwork", "REPO")]
+
+namespace Repo_Library
+{
+    public class SharedData
+    {
+        public static bool IsInMainMenu { get; set; }
+        public static bool IsInitialized { get; set; }
+        public static bool IsInLobby { get; set; }
+        public static bool IsInGame { get; set; }
+        public static List<Level> Levels = new List<Level>();
+        public static List<Level> Menus = new List<Level>();
+    }
+
+    public class SharedPlayerData
+    {
+        public static PlayerController PlayerController { get; set; }
+        public static PlayerCollision PlayerCollision { get; set; }
+    }
+
+    public class Library : MelonMod
+    {
+        public async void SetPlayerData()
+        {
+            await Task.Delay(1000);
+            // Set controllers for the player
+            GameObject player = GameObject.Find("Player").transform.Find("Controller").gameObject;
+            GameObject collision = player.transform.Find("Collision").gameObject;
+            if (player == null)
+            {
+                MelonLogger.Msg("Player not found");
+                return;
+            }
+            else
+            {
+                MelonLogger.Msg("Player found");
+            }
+
+            PlayerController playerController = player.GetComponent<PlayerController>();
+            PlayerCollision playerCollision = collision.GetComponent<PlayerCollision>();
+
+            if (playerController == null)
+            {
+                MelonLogger.Msg("Player controller not found");
+                return;
+            }
+
+            if (playerCollision == null)
+            {
+                MelonLogger.Msg("Player collision not found");
+                return;
+            }
+
+            SetPlayerController(playerController);
+            SetPlayerCollision(playerCollision);
+        }
+
+        public override void OnSceneWasInitialized(int buildIndex, string sceneName)
+        {
+            RunManager runManager = GameObject.Find("Run Manager").GetComponent<RunManager>();
+
+            // Set and store levels for the game
+            if (!IsInitialized())
+            {
+                SetInititialized(true);
+                SetLevels(runManager.levels);
+                SetMenuLevels(new List<Level> { runManager.levelMainMenu, runManager.levelLobbyMenu, runManager.levelLobby });
+            }
+
+            // Check if the player is in the main menu
+            if (runManager.levelCurrent == runManager.levelMainMenu)
+            {
+                SetInMainMenu(true);
+            }
+            else
+            {
+                SetInMainMenu(false);
+            }
+
+            // Check if the player is in the lobby
+            if (runManager.levelCurrent == runManager.levelLobbyMenu || runManager.levelCurrent == runManager.levelLobby)
+            {
+                SetInLobby(true);
+            }
+            else
+            {
+                SetInLobby(false);
+            }
+
+            // Checks if the player is in game
+            if (!SharedData.Menus.Contains(runManager.levelCurrent))
+            {
+                // Check if we are already in a game
+                if (IsInGame()) return;
+                SetInGame(true);
+                SetPlayerData();
+            }
+            else
+            {
+                SetInGame(false);
+                SetPlayerController(null);
+                SetPlayerCollision(null);
+            }
+        }
+        
+        // SET METHODS
+
+        public void SetPlayerController(PlayerController playerController)
+        {
+            SharedPlayerData.PlayerController = playerController ?? null;
+        }
+
+        public void SetPlayerCollision(PlayerCollision playerCollision)
+        {
+            SharedPlayerData.PlayerCollision = playerCollision ?? null;
+        }
+
+        public void SetInititialized(bool value)
+        {
+            SharedData.IsInitialized = value;
+        }
+
+        public void SetInMainMenu(bool value)
+        {
+            SharedData.IsInMainMenu = value;
+        }
+
+        public void SetInLobby(bool value)
+        {
+            SharedData.IsInLobby = value;
+        }
+
+        public void SetLevels(List<Level> levels)
+        {
+            SharedData.Levels = levels;
+        }
+
+        public void SetMenuLevels(List<Level> levels)
+        {
+            SharedData.Menus = levels;
+        }
+
+        public void SetInGame(bool value)
+        {
+            SharedData.IsInGame = value;
+        }
+
+        // GET METHODS
+
+        public bool IsInitialized()
+        {
+            return SharedData.IsInitialized;
+        }
+
+        public bool IsInMainMenu()
+        {
+            return SharedData.IsInMainMenu;
+        }
+
+        public bool IsInLobby()
+        {
+            return SharedData.IsInLobby;
+        }
+
+        public List<Level> GetLevels()
+        {
+            return SharedData.Levels;
+        }
+
+        public bool IsInGame() 
+        { 
+            return SharedData.IsInGame; 
+        }
+
+        public List<Level> GetMenuLevels()
+        {
+            return SharedData.Menus;
+        }
+
+        public PlayerController GetPlayerController()
+        {
+            return SharedPlayerData.PlayerController;
+        }
+
+        public PlayerCollision GetPlayerCollision()
+        {
+            return SharedPlayerData.PlayerCollision;
+        }
+
+        // Revive player at a spawn point
+        public void RevivePlayer (PlayerController playerController)
+        {
+            Vector3 respawn = new Vector3(0f, 0f, -21f);
+            if (playerController != null) return;
+            playerController?.Revive(respawn);
+        }
+
+        // Respawn player at a specific position
+        public void RespawnPlayer(PlayerController playerController)
+        {
+            Vector3 respawn = new Vector3(0f, 0f, -21f);
+            if (playerController == null) return;
+            playerController.gameObject.transform.position = respawn;
+        }
+
+        // Teleport player to a vector3 position
+        public void TeleportPlayer(PlayerController playerController, Vector3 position)
+        {
+            if (playerController == null) return;
+            playerController.gameObject.transform.position = position;
+        }
+
+        // Set player current energy to a specific value
+        public void SetPlayerCurrentEnergy(PlayerController playerController, float energy)
+        {
+            if (playerController == null) return;
+            playerController.EnergyCurrent = energy;
+        }
+
+        // Set player max energy to a specific value
+        public void SetPlayerMaxEnergy(PlayerController playerController, float energy)
+        {
+            if (playerController == null) return;
+            playerController.EnergyStart = energy;
+        }
+
+        // Set sprint energy drain to a specific value
+        public void SetSprintEnergyDrain(PlayerController playerController, float energy)
+        {
+            if (playerController == null) return;
+            playerController.EnergySprintDrain = energy;
+        }
+
+        // Get player max energy
+        public float GetPlayerMaxEnergy(PlayerController playerController)
+        {
+            if (playerController == null) return 0f;
+            return playerController.EnergyStart;
+        }
+
+        // Add anti-gravity to the player
+        public void AntiGravity(PlayerController playerController, float time)
+        {
+            if (playerController == null) return;
+            playerController.AntiGravity(time);
+        }
+
+        // Set crounch speed to a specific value
+        public void SetCrouchSpeed(PlayerController playerController, float speed)
+        {
+            if (playerController == null) return;
+            playerController.CrouchSpeed = speed;
+        }
+
+        // Set movement speed to a specific value
+        public void SetMovementSpeed(PlayerController playerController, float speed)
+        {
+            if (playerController == null) return;
+            playerController.MoveSpeed = speed;
+        }
+
+        // Set sprint speed to a specific value
+        public void SetSprintSpeed(PlayerController playerController, float speed)
+        {
+            if (playerController == null) return;
+            playerController.SprintSpeed = speed;
+        }
+
+        // Check if the player is sprinting
+        public bool IsSprinting(PlayerController playerController)
+        {
+            if (playerController == null) return false;
+            return playerController.sprinting;
+        }
+
+        // Set sprinting to a specific value
+        public void SetSprinting(PlayerController playerController, bool value)
+        {
+            if (playerController == null) return;
+            playerController.sprinting = value;
+        }
+
+        // Set speed upgrade amounts to a specific value
+        public void SetUpgradeAmounts(PlayerController playerController, int amount)
+        {
+            if (playerController == null) return;
+            playerController.SprintSpeedUpgrades = amount;
+        }
+
+        // Set custom gravity to a specific value
+        public void SetCustomGravity(PlayerController playerController, float gravity)
+        {
+            if (playerController == null) return;
+            playerController.CustomGravity = gravity;
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("(Official) R.E.P.O Mod Library")]
+[assembly: AssemblyDescription("The official mod library for the game R.E.P.O")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("(Official) R.E.P.O Mod Library")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1b092381-d16a-418d-8cfe-b28db50bfeb0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
